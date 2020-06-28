26120c6b8b3c9d714db753963760ee6809919f29
Aleksei Semenov <7.on.off@bk.ru>
2020-06-28 11:33:24 +0300

    test github action

--- fixapp/tests.py
+++ fixapp/tests.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+from django.apps import apps
 from django.test import TestCase
 
-# Create your tests here.
+from .apps import FixappConfig
+
+
+class TestsFixappConfig(TestCase):
+    def test_app(self: TestsFixappConfig) -> None:
+        assert "fixapp" == FixappConfig.name
+        assert "fixapp" == apps.get_app_config("fixapp").name
